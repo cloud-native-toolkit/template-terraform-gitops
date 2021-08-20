dfde36d40274e1dcecf5bbd8e7e0bb9bfe1fdf5c
Sean Sundberg <seansund@us.ibm.com>
2021-08-20 14:42:35 -0500

    Updates template to include addl variables and use igc cli
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-mymodule.tf
+++ test/stages/stage2-mymodule.tf
@@ -1,10 +1,12 @@
-module "gitops_pactbroker" {
+module "gitops_module" {
   source = "./module"
 
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
+  server_name = module.gitops.server_name
   namespace = module.gitops_namespace.name
   cluster_ingress_hostname = module.dev_cluster.platform.ingress
-  cluster_type             = module.dev_cluster.platform.type_code
-  tls_secret_name          = module.dev_cluster.platform.tls_secret
+  cluster_type = module.dev_cluster.platform.type_code
+  tls_secret_name = module.dev_cluster.platform.tls_secret
+  kubeseal_cert = module.argocd-bootstrap.sealed_secrets_cert
 }

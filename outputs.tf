2218717457e4d8e20d44c96b40acde6b8a96a746
Sean Sundberg <seansund@us.ibm.com>
2022-01-06 08:33:01 -0600

    Updates readme and default tests for usability (#8)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,36 @@
-#output "myoutput" {
-#  description = "Description of my output"
-#  value       = "value"
-#  depends_on  = [<some resource>]
-#}
+
+output "name" {
+  description = "The name of the module"
+  value       = local.name
+  depends_on  = [null_resource.setup_gitops]
+}
+
+output "branch" {
+  description = "The branch where the module config has been placed"
+  value       = local.application_branch
+  depends_on  = [null_resource.setup_gitops]
+}
+
+output "namespace" {
+  description = "The namespace where the module will be deployed"
+  value       = local.namespace
+  depends_on  = [null_resource.setup_gitops]
+}
+
+output "server_name" {
+  description = "The server where the module will be deployed"
+  value       = var.server_name
+  depends_on  = [null_resource.setup_gitops]
+}
+
+output "layer" {
+  description = "The layer where the module is deployed"
+  value       = local.layer
+  depends_on  = [null_resource.setup_gitops]
+}
+
+output "type" {
+  description = "The type of module where the module is deployed"
+  value       = local.type
+  depends_on  = [null_resource.setup_gitops]
+}

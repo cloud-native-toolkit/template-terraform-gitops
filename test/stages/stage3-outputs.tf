a5ceac761288309ce8a564a3a7502dcbd70e4b24
Andrew Trice <amtrice@us.ibm.com>
2022-01-20 15:12:52 -0500

    changed single quotes to escaped double quotes to avoid failures (#14)
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- test/stages/stage3-outputs.tf
+++ test/stages/stage3-outputs.tf
@@ -1,7 +1,7 @@
 
 resource null_resource write_outputs {
   provisioner "local-exec" {
-    command = "echo '$${OUTPUT}' > gitops-output.json"
+    command = "echo \"$${OUTPUT}\" > gitops-output.json"
 
     environment = {
       OUTPUT = jsonencode({

7776aea5f392710b765fe40213f98f2994e440dd
Andrew Trice <amtrice@us.ibm.com>
2022-02-10 09:41:49 -0500

    updated test matrix to support both AWS and IBM Cloud
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- test/stages/stage1-cluster.tf
+++ test/stages/stage1-cluster.tf
@@ -2,8 +2,8 @@ module "dev_cluster" {
   source = "github.com/cloud-native-toolkit/terraform-ocp-login.git"
 
   server_url = var.server_url
-  login_user = "apikey"
-  login_password = var.ibmcloud_api_key
+  login_user = var.cluster_username
+  login_password = var.cluster_password
   login_token = ""
 }
 
--- test/stages/variables.tf
+++ test/stages/variables.tf
@@ -1,13 +1,12 @@
 
-# Resource Group Variables
-variable "resource_group_name" {
+variable cluster_username { 
   type        = string
-  description = "Existing resource group where the IKS cluster will be provisioned."
+  description = "The username for AWS access"
 }
 
-variable "ibmcloud_api_key" {
+variable "cluster_password" {
   type        = string
-  description = "The api key for IBM Cloud access"
+  description = "The password for AWS access"
 }
 
 variable "server_url" {
@@ -39,18 +38,6 @@ variable "cluster_exists" {
   type        = string
   description = "Flag indicating if the cluster already exists (true or false)"
   default     = "true"
-}
-
-variable "name_prefix" {
-  type        = string
-  description = "Prefix name that should be used for the cluster and services. If not provided then resource_group_name will be used"
-  default     = ""
-}
-
-variable "vpc_cluster" {
-  type        = bool
-  description = "Flag indicating that this is a vpc cluster"
-  default     = false
 }
 
 variable "git_token" {

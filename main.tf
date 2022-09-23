dbf220dbe9217f1750461e0cac25c378687db95b
Sean Sundberg <seansund@us.ibm.com>
2022-09-23 11:19:20 -0500

    Updates the module to use gitops provider
    - Also updates test to use gitea by default
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 locals {
   name          = "my-module"
-  bin_dir       = module.setup_clis.bin_dir
   yaml_dir      = "${path.cwd}/.tmp/${local.name}/chart/${local.name}"
   service_url   = "http://${local.name}.${var.namespace}"
   values_content = {
@@ -10,10 +9,6 @@ locals {
   application_branch = "main"
   namespace = var.namespace
   layer_config = var.gitops_config[local.layer]
-}
-
-module setup_clis {
-  source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
 }
 
 resource null_resource create_yaml {
@@ -26,37 +21,16 @@ resource null_resource create_yaml {
   }
 }
 
-resource null_resource setup_gitops {
+resource gitops_module module {
   depends_on = [null_resource.create_yaml]
 
-  triggers = {
-    name = local.name
-    namespace = var.namespace
-    yaml_dir = local.yaml_dir
-    server_name = var.server_name
-    layer = local.layer
-    type = local.type
-    git_credentials = yamlencode(var.git_credentials)
-    gitops_config   = yamlencode(var.gitops_config)
-    bin_dir = local.bin_dir
-  }
-
-  provisioner "local-exec" {
-    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
-
-    environment = {
-      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
-      GITOPS_CONFIG   = self.triggers.gitops_config
-    }
-  }
-
-  provisioner "local-exec" {
-    when = destroy
-    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --delete --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
-
-    environment = {
-      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
-      GITOPS_CONFIG   = self.triggers.gitops_config
-    }
-  }
+  name = local.name
+  namespace = var.namespace
+  content_dir = local.yaml_dir
+  server_name = var.server_name
+  layer = local.layer
+  type = local.type
+  branch = local.application_branch
+  config = yamlencode(var.gitops_config)
+  credentials = yamlencode(var.git_credentials)
 }
--- test/stages/stage1-gitops.tf
+++ test/stages/stage1-gitops.tf
@@ -1,13 +1,29 @@
-module "gitops" {
-  source = "github.com/cloud-native-toolkit/terraform-tools-gitops"
+module "dev_tools_namespace" {
+  source = "github.com/cloud-native-toolkit/terraform-k8s-namespace.git"
 
-  host = var.git_host
-  type = var.git_type
-  org  = var.git_org
+  cluster_config_file_path = module.dev_cluster.config_file_path
+  name                     = var.namespace
+}
+
+module "gitea" {
+  source = "github.com/cloud-native-toolkit/terraform-tools-gitea"
+
+  cluster_config_file = module.dev_cluster.config_file_path
+  ca_cert             = module.dev_cluster.ca_cert
+  olm_namespace       = module.dev_software_olm.olm_namespace
+  operator_namespace  = module.dev_software_olm.target_namespace
+  instance_namespace  = module.dev_tools_namespace.name
+}
+
+module "gitops" {
+  source = "github.com/cloud-native-toolkit/terraform-tools-gitops.git"
+
+  gitea_host = module.gitea.host
+  gitea_org = module.gitea.org
+  gitea_username = module.gitea.username
+  gitea_token = module.gitea.token
   repo = var.git_repo
-  token = var.git_token
   public = true
-  username = var.git_username
   gitops_namespace = var.gitops_namespace
   sealed_secrets_cert = module.cert.cert
 }
